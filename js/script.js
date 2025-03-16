e5e1c0b79ec0febba639398b6235b4a9f199c075
MonjurElahiTouheed <monjurelahitouheed@gmail.com>
2025-03-16 10:30:24 +0600

    input field targeted

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const result = document.querySelector('.result');
+const weight = document.querySelector('.weight__field').value;
+const height = document.querySelector('.height__field').value/100;
+const calculate = document.querySelector('.calculate');
+
+calculate.addEventListener('click', () => {
+    
+})
